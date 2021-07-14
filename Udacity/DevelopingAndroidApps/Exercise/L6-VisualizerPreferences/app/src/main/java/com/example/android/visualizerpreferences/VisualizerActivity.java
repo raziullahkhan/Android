76ab649a072401ab08a8024047d85dff3d2e0840
Raziullah <raziullahkhan2222@gmail.com>
2021-07-14 13:13:27 +0530

    Resoving hardcoded string

--- Udacity/DevelopingAndroidApps/Exercise/L6-VisualizerPreferences/app/src/main/java/com/example/android/visualizerpreferences/VisualizerActivity.java
+++ Udacity/DevelopingAndroidApps/Exercise/L6-VisualizerPreferences/app/src/main/java/com/example/android/visualizerpreferences/VisualizerActivity.java
@@ -36,7 +36,7 @@ public class VisualizerActivity extends AppCompatActivity {
     private void setupSharedPreferences() {
         SharedPreferences sharedPreferences= PreferenceManager.getDefaultSharedPreferences(this);
 
-        mVisualizerView.setShowBass(sharedPreferences.getBoolean("show_bass",true));
+        mVisualizerView.setShowBass(sharedPreferences.getBoolean(getString(R.string.pref_show_bass_key),getResources().getBoolean(R.bool.pref_show_bass_default)));
         mVisualizerView.setShowMid(true);
         mVisualizerView.setShowTreble(true);
         mVisualizerView.setMinSizeScale(1);
