8eb86f9ba3e94f264afc4cadf5467a9c9215a47c
Raziullah <raziullahkhan2222@gmail.com>
2021-07-17 17:34:17 +0530

    Added an IntentService

--- Udacity/DevelopingAndroidApps/Exercise/L9-HydrationReminder/app/src/main/java/com/example/android/background/MainActivity.java
+++ Udacity/DevelopingAndroidApps/Exercise/L9-HydrationReminder/app/src/main/java/com/example/android/background/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.background;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -10,6 +11,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.android.background.sync.ReminderTasks;
+import com.example.android.background.sync.WaterReminderIntentService;
 import com.example.android.background.utilities.PreferenceUtilities;
 
 public class MainActivity extends AppCompatActivity implements
@@ -54,7 +57,9 @@ public class MainActivity extends AppCompatActivity implements
         if (mToast != null) mToast.cancel();
         mToast = Toast.makeText(this, R.string.water_chug_toast, Toast.LENGTH_SHORT);
         mToast.show();
-
+        Intent incrementWaterCountIntent=new Intent(this, WaterReminderIntentService.class);
+        incrementWaterCountIntent.setAction(ReminderTasks.ACTION_INCREMENT_WATER_COUNT);
+        startService(incrementWaterCountIntent);
     }
 
     @Override
