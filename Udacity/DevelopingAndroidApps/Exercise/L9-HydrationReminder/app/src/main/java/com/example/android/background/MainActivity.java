57bf23d22dcf1358441ca873ef5f0e448325744c
Raziullah <raziullahkhan2222@gmail.com>
2021-07-17 16:59:04 +0530

    Implemented starter code

---
+++ Udacity/DevelopingAndroidApps/Exercise/L9-HydrationReminder/app/src/main/java/com/example/android/background/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.android.background;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.android.background.utilities.PreferenceUtilities;
+
+public class MainActivity extends AppCompatActivity implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private TextView mWaterCountDisplay;
+    private TextView mChargingCountDisplay;
+    private ImageView mChargingImageView;
+
+    private Toast mToast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWaterCountDisplay = (TextView) findViewById(R.id.tv_water_count);
+        mChargingCountDisplay = (TextView) findViewById(R.id.tv_charging_reminder_count);
+        mChargingImageView = (ImageView) findViewById(R.id.iv_power_increment);
+
+        updateWaterCount();
+        updateChargingReminderCount();
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        prefs.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    private void updateWaterCount() {
+        int waterCount = PreferenceUtilities.getWaterCount(this);
+        mWaterCountDisplay.setText(waterCount+"");
+    }
+
+    private void updateChargingReminderCount() {
+        int chargingReminders = PreferenceUtilities.getChargingReminderCount(this);
+        String formattedChargingReminders = getResources().getQuantityString(
+                R.plurals.charge_notification_count, chargingReminders, chargingReminders);
+        mChargingCountDisplay.setText(formattedChargingReminders);
+
+    }
+
+    public void incrementWater(View view) {
+        if (mToast != null) mToast.cancel();
+        mToast = Toast.makeText(this, R.string.water_chug_toast, Toast.LENGTH_SHORT);
+        mToast.show();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        prefs.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (PreferenceUtilities.KEY_WATER_COUNT.equals(key)) {
+            updateWaterCount();
+        } else if (PreferenceUtilities.KEY_CHARGING_REMINDER_COUNT.equals(key)) {
+            updateChargingReminderCount();
+        }
+    }
+}
