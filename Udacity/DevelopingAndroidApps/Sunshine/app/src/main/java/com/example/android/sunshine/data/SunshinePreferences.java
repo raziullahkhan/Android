e286f2e43c0cd591ede2aac269aa59121cfb1f8b
Raziullah <raziullahkhan2222@gmail.com>
2021-07-18 16:38:46 +0530

    Added code for notification

--- Udacity/DevelopingAndroidApps/Sunshine/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
+++ Udacity/DevelopingAndroidApps/Sunshine/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
@@ -103,6 +103,22 @@ public class SunshinePreferences {
 
         return spContainBothLatitudeAndLongitude;
     }
+    public static boolean areNotificationsEnabled(Context context) {
+
+        String displayNotificationsKey = context.getString(R.string.pref_enable_notifications_key);
+
+        boolean shouldDisplayNotificationsByDefault = context
+                .getResources()
+                .getBoolean(R.bool.show_notifications_by_default);
+
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+
+        boolean shouldDisplayNotifications = sp
+                .getBoolean(displayNotificationsKey, shouldDisplayNotificationsByDefault);
+
+        return shouldDisplayNotifications;
+    }
+
     public static long getLastNotificationTimeInMillis(Context context) {
         String lastNotificationKey = context.getString(R.string.pref_last_notification);
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
