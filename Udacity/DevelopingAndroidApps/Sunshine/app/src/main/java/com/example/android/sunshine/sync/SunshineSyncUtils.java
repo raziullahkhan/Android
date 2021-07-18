ebd3c50a5cd1fa623acdf9a3b48e90eb0fee0403
Raziullah <raziullahkhan2222@gmail.com>
2021-07-18 16:10:30 +0530

    Smarter Syncing

--- Udacity/DevelopingAndroidApps/Sunshine/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ Udacity/DevelopingAndroidApps/Sunshine/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -2,10 +2,41 @@ package com.example.android.sunshine.sync;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
 
 import androidx.annotation.NonNull;
 
+import com.example.android.sunshine.data.WeatherContract;
+
 public class SunshineSyncUtils {
+    private static boolean sInitialized;
+    synchronized public static void initialize(@NonNull final Context context) {
+        if (sInitialized) return;
+        sInitialized = true;
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            public Void doInBackground( Void... voids ) {
+
+                Uri forecastQueryUri = WeatherContract.WeatherEntry.CONTENT_URI;
+                String[] projectionColumns = {WeatherContract.WeatherEntry._ID};
+                String selectionStatement = WeatherContract.WeatherEntry
+                        .getSqlSelectForTodayOnwards();
+                Cursor cursor = context.getContentResolver().query(
+                        forecastQueryUri,
+                        projectionColumns,
+                        selectionStatement,
+                        null,
+                        null);
+                if (null == cursor || cursor.getCount() == 0) {
+                    startImmediateSync(context);
+                }
+                cursor.close();
+                return null;
+            }
+        }.execute();
+    }
 
     public static void startImmediateSync(@NonNull final Context context) {
         Intent intentToSyncImmediately = new Intent(context, SunshineSyncIntentService.class);
