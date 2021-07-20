410ca113b668e0cea291916085c45562678629c0
Raziullah <raziullahkhan2222@gmail.com>
2021-07-20 12:47:43 +0530

    Added initial code

---
+++ Udacity/AdvancedAndroidAppDeveopment/Android_Me/app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.android.android_me.R;
+
+public class AndroidMeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_me);
+    }
+}
