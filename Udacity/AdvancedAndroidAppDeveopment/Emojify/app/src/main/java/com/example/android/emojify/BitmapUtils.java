2252245fde1e83845f14c9b8e782357735af3432
Raziullah <raziullahkhan2222@gmail.com>
2021-07-25 20:19:42 +0530

    Starter Code

---
+++ Udacity/AdvancedAndroidAppDeveopment/Emojify/app/src/main/java/com/example/android/emojify/BitmapUtils.java
@@ -0,0 +1,119 @@
+package com.example.android.emojify;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+class BitmapUtils {
+
+    private static final String FILE_PROVIDER_AUTHORITY = "com.example.android.fileprovider";
+
+    static Bitmap resamplePic(Context context, String imagePath) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        manager.getDefaultDisplay().getMetrics(metrics);
+
+        int targetH = metrics.heightPixels;
+        int targetW = metrics.widthPixels;
+
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imagePath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        return BitmapFactory.decodeFile(imagePath);
+    }
+
+    static File createTempImageFile(Context context) throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = context.getExternalCacheDir();
+
+        return File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+    }
+
+    static boolean deleteImageFile(Context context, String imagePath) {
+        File imageFile = new File(imagePath);
+        boolean deleted = imageFile.delete();
+        if (!deleted) {
+            String errorMessage = context.getString(R.string.error);
+            Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show();
+        }
+        return deleted;
+    }
+
+    private static void galleryAddPic(Context context, String imagePath) {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(imagePath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        context.sendBroadcast(mediaScanIntent);
+    }
+
+    static String saveImage(Context context, Bitmap image) {
+        String savedImagePath = null;
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + ".jpg";
+        File storageDir = new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                        + "/Emojify");
+        boolean success = true;
+        if (!storageDir.exists()) {
+            success = storageDir.mkdirs();
+        }
+
+        if (success) {
+            File imageFile = new File(storageDir, imageFileName);
+            savedImagePath = imageFile.getAbsolutePath();
+            try {
+                OutputStream fOut = new FileOutputStream(imageFile);
+                image.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+                fOut.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            galleryAddPic(context, savedImagePath);
+            String savedMessage = context.getString(R.string.saved_message, savedImagePath);
+            Toast.makeText(context, savedMessage, Toast.LENGTH_SHORT).show();
+        }
+
+        return savedImagePath;
+    }
+
+    static void shareImage(Context context, String imagePath) {
+        File imageFile = new File(imagePath);
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("image/*");
+        Uri photoURI = FileProvider.getUriForFile(context, FILE_PROVIDER_AUTHORITY, imageFile);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, photoURI);
+        context.startActivity(shareIntent);
+    }
+}
---
+++ Udacity/AdvancedAndroidAppDeveopment/Emojify/app/src/main/java/com/example/android/emojify/MainActivity.java
@@ -0,0 +1,149 @@
+package com.example.android.emojify;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_STORAGE_PERMISSION = 1;
+
+    private static final String FILE_PROVIDER_AUTHORITY = "com.example.android.fileprovider";
+
+    private ImageView mImageView;
+
+    private Button mEmojifyButton;
+    private FloatingActionButton mShareFab;
+    private FloatingActionButton mSaveFab;
+    private FloatingActionButton mClearFab;
+
+    private TextView mTitleTextView;
+
+    private String mTempPhotoPath;
+
+    private Bitmap mResultsBitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mImageView = (ImageView) findViewById(R.id.image_view);
+        mEmojifyButton = (Button) findViewById(R.id.emojify_button);
+        mShareFab = (FloatingActionButton) findViewById(R.id.share_button);
+        mSaveFab = (FloatingActionButton) findViewById(R.id.save_button);
+        mClearFab = (FloatingActionButton) findViewById(R.id.clear_button);
+        mTitleTextView = (TextView) findViewById(R.id.title_text_view);
+    }
+    public void emojifyMe(View view) {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_STORAGE_PERMISSION);
+        } else {
+            launchCamera();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        switch (requestCode) {
+            case REQUEST_STORAGE_PERMISSION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    launchCamera();
+                } else {
+                    Toast.makeText(this, R.string.permission_denied, Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
+    }
+
+    private void launchCamera() {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = BitmapUtils.createTempImageFile(this);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            if (photoFile != null) {
+                mTempPhotoPath = photoFile.getAbsolutePath();
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        FILE_PROVIDER_AUTHORITY,
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            // Process the image and set it to the TextView
+            processAndSetImage();
+        } else {
+            BitmapUtils.deleteImageFile(this, mTempPhotoPath);
+        }
+    }
+    private void processAndSetImage() {
+
+        mEmojifyButton.setVisibility(View.GONE);
+        mTitleTextView.setVisibility(View.GONE);
+        mSaveFab.setVisibility(View.VISIBLE);
+        mShareFab.setVisibility(View.VISIBLE);
+        mClearFab.setVisibility(View.VISIBLE);
+        mResultsBitmap = BitmapUtils.resamplePic(this, mTempPhotoPath);
+        mImageView.setImageBitmap(mResultsBitmap);
+    }
+
+    public void saveMe(View view) {
+        BitmapUtils.deleteImageFile(this, mTempPhotoPath);
+        BitmapUtils.saveImage(this, mResultsBitmap);
+    }
+
+    public void shareMe(View view) {
+        BitmapUtils.deleteImageFile(this, mTempPhotoPath);
+        BitmapUtils.saveImage(this, mResultsBitmap);
+        BitmapUtils.shareImage(this, mTempPhotoPath);
+    }
+    public void clearImage(View view) {
+        mImageView.setImageResource(0);
+        mEmojifyButton.setVisibility(View.VISIBLE);
+        mTitleTextView.setVisibility(View.VISIBLE);
+        mShareFab.setVisibility(View.GONE);
+        mSaveFab.setVisibility(View.GONE);
+        mClearFab.setVisibility(View.GONE);
+        BitmapUtils.deleteImageFile(this, mTempPhotoPath);
+    }
+}
