ad9803e7d9e1037f2e6d2086d8cefc63266b67f7
Raziullah <raziullahkhan2222@gmail.com>
2021-07-25 15:03:30 +0530

    Implemented communication between activities

--- Udacity/AdvancedAndroidAppDeveopment/Android_Me/app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ Udacity/AdvancedAndroidAppDeveopment/Android_Me/app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.android.android_me.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.android.android_me.R;
 
 public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener{
+
+    private int headIndex;
+    private int bodyIndex;
+    private int legIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +25,29 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
     @Override
     public void onImageSelected(int position) {
         Toast.makeText(this, "Position clicked = " + position, Toast.LENGTH_SHORT).show();
+        int bodyPartNumber=position/12;
+        int listIndex=position-12*bodyPartNumber;
+        switch (bodyPartNumber){
+            case 0: headIndex=listIndex;
+                break;
+            case 1: bodyIndex=listIndex;
+                break;
+            case 2: legIndex=listIndex;
+                break;
+            default: break;
+        }
+        Bundle b=new Bundle();
+        b.putInt("headIndex",headIndex);
+        b.putInt("bodyIndex",bodyIndex);
+        b.putInt("legIndex",legIndex);
+        final Intent intent=new Intent(this,AndroidMeActivity.class);
+        intent.putExtras(b);
+        Button nextButton=(Button)findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 }
