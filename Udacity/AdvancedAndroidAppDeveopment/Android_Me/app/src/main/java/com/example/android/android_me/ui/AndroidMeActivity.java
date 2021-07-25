3e235f117155f00c244912c5daacaa511eaf9074
Raziullah <raziullahkhan2222@gmail.com>
2021-07-25 12:01:36 +0530

    Created a Fragment

--- Udacity/AdvancedAndroidAppDeveopment/Android_Me/app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ Udacity/AdvancedAndroidAppDeveopment/Android_Me/app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -3,6 +3,7 @@ package com.example.android.android_me.ui;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.android.android_me.R;
 
@@ -11,5 +12,9 @@ public class AndroidMeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_me);
+        BodyPartFragment headFragment=new BodyPartFragment();
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        fragmentManager.beginTransaction().add(R.id.head_container,headFragment).commit();
+
     }
 }
