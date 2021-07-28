288fcb278de5c9a576e6c2526261a0acdcb880db
Raziullah <raziullahkhan2222@gmail.com>
2021-07-28 14:03:39 +0530

    Starting Code for ShushMe

---
+++ Udacity/AdvancedAndroidAppDeveopment/ShushMe/app/src/main/java/com/example/android/shushme/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.android.shushme;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Constants
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    // Member variables
+    private PlaceListAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set up the recycler view
+        mRecyclerView = (RecyclerView) findViewById(R.id.places_list_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new PlaceListAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+}
