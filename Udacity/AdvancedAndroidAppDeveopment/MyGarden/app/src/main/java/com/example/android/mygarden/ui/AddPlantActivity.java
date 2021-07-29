5aca67c7e434931c5249c06dc76669664a075f1f
Raziullah <raziullahkhan2222@gmail.com>
2021-07-29 18:20:22 +0530

    Starting Code for MyGarden App

---
+++ Udacity/AdvancedAndroidAppDeveopment/MyGarden/app/src/main/java/com/example/android/mygarden/ui/AddPlantActivity.java
@@ -0,0 +1,53 @@
+package com.example.android.mygarden.ui;
+
+import android.content.ContentValues;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.android.mygarden.R;
+import com.example.android.mygarden.provider.PlantContract;
+
+public class AddPlantActivity extends AppCompatActivity {
+    private RecyclerView mTypesRecyclerView;
+    private PlantTypesAdapter mTypesAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_plant);
+
+        // Plant types are displayed as a recycler view using PlantTypesAdapter
+        mTypesAdapter = new PlantTypesAdapter(this);
+        mTypesRecyclerView = (RecyclerView) findViewById(R.id.plant_types_recycler_view);
+        mTypesRecyclerView.setLayoutManager(
+                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        );
+        mTypesRecyclerView.setAdapter(mTypesAdapter);
+
+    }
+    public void onPlantTypeClick(View view) {
+        // When the chosen plant type is clicked, create a new plant and set the creation time and
+        // water time to now
+        // Extract the plant type from the tag
+        ImageView imgView = (ImageView) view.findViewById(R.id.plant_type_image);
+        int plantType = (int) imgView.getTag();
+        long timeNow = System.currentTimeMillis();
+        // Insert the new plant into DB
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(PlantContract.PlantEntry.COLUMN_PLANT_TYPE, plantType);
+        contentValues.put(PlantContract.PlantEntry.COLUMN_CREATION_TIME, timeNow);
+        contentValues.put(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME, timeNow);
+        getContentResolver().insert(PlantContract.PlantEntry.CONTENT_URI, contentValues);
+        // Close this activity
+        finish();
+    }
+
+    public void onBackButtonClick(View view) {
+        finish();
+    }
+}
