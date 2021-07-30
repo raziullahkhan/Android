787b4fb5b549d3907b1dc877efd184dc241d8b19
Raziullah <raziullahkhan2222@gmail.com>
2021-07-31 03:12:54 +0530

    Starting Code for TeaTime App

---
+++ Udacity/AdvancedAndroidAppDeveopment/TeaTime/app/src/main/java/com/example/android/teatime/model/Tea.java
@@ -0,0 +1,22 @@
+package com.example.android.teatime.model;
+
+public class Tea {
+
+    private String mTeaName;
+    private int mImageResourceId;
+
+    public Tea(String teaName, int imageResourceId) {
+        mTeaName = teaName;
+        mImageResourceId = imageResourceId;
+    }
+
+    public String getTeaName() {
+        return mTeaName;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+}
+
