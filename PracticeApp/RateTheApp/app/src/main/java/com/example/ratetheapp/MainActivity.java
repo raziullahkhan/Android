567a008cf30b5334f6af08302de590679f0a4b46
Raziullah <raziullahkhan2222@gmail.com>
2021-09-17 14:39:01 +0530

    Implemented Rate the app dialog box

--- PracticeApp/RateTheApp/app/src/main/java/com/example/ratetheapp/MainActivity.java
+++ PracticeApp/RateTheApp/app/src/main/java/com/example/ratetheapp/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import hotchemi.android.rate.AppRate;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppRate.with(this).setInstallDays(0).setLaunchTimes(1)
+                .setRemindInterval(1).monitor();
+        AppRate.showRateDialogIfMeetsConditions(this);
     }
 }
