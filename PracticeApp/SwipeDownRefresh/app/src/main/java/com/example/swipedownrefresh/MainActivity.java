32f78fe1c82491a35451ed3deec5e6402a750e30
Raziullah <raziullahkhan2222@gmail.com>
2021-09-18 17:56:05 +0530

    Implemented Refreshing

--- PracticeApp/SwipeDownRefresh/app/src/main/java/com/example/swipedownrefresh/MainActivity.java
+++ PracticeApp/SwipeDownRefresh/app/src/main/java/com/example/swipedownrefresh/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.swipedownrefresh;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SwipeRefreshLayout swipeRefreshLayout=(SwipeRefreshLayout) findViewById(R.id.refreshLayout);
+        TextView textView=(TextView)findViewById(R.id.tv1);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                textView.setText("Refreshed");
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
     }
 }
