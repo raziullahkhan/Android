09eef19cfaec4adaf633896820640fcdc0272a82
Raziullah <raziullahkhan2222@gmail.com>
2021-09-19 15:49:57 +0530

    Implemented Text To Speech

--- PracticeApp/TextToSpeech/app/src/main/java/com/example/texttospeech/MainActivity.java
+++ PracticeApp/TextToSpeech/app/src/main/java/com/example/texttospeech/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.texttospeech;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText text;
+    Button button;
+    TextToSpeech textToSpeech;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text=findViewById(R.id.Text);
+        button=findViewById(R.id.btnText);
+
+        textToSpeech=new TextToSpeech(getApplicationContext(),
+                new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        if(status!=TextToSpeech.ERROR){
+                            textToSpeech.setLanguage(Locale.UK);
+                        }
+                    }
+                });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textToSpeech.speak(text.getText().toString(),
+                        TextToSpeech.QUEUE_FLUSH,null);
+            }
+        });
     }
 }
