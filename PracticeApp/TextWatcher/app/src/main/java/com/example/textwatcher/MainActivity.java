dcc60e9ba74f9d4ce9979ccd487c60d9269579b3
Raziullah <raziullahkhan2222@gmail.com>
2021-09-28 12:58:35 +0530

    Implemented TextWatcher

--- PracticeApp/TextWatcher/app/src/main/java/com/example/textwatcher/MainActivity.java
+++ PracticeApp/TextWatcher/app/src/main/java/com/example/textwatcher/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.textwatcher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etEmail, etPassword;
+    Button bLogin;
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String emailInput = etEmail.getText().toString();
+            String passwordInput = etPassword.getText().toString();
+            bLogin.setEnabled(!emailInput.isEmpty() && !passwordInput.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        bLogin = findViewById(R.id.loginButton);
+        etEmail.addTextChangedListener(textWatcher);
+        etPassword.addTextChangedListener(textWatcher);
     }
 }
