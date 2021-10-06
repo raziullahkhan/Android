1f2810bc07276e1e64e79871152638a9fec9ea3e
Md Raziullah Khan <raziullahkhan2222@gmail.com>
2021-10-06 14:36:44 +0530

    Implemented Form Validation

--- PracticeApp/FormValidation/app/src/main/java/com/example/formvalidation/MainActivity.java
+++ PracticeApp/FormValidation/app/src/main/java/com/example/formvalidation/MainActivity.java
@@ -2,13 +2,68 @@ package com.example.formvalidation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button bCancel, bProceed;
+    EditText etFirstName, etLastName, etEmail, etPassword;
+    boolean isAllFieldsChecked = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bProceed = findViewById(R.id.proceedButton);
+        bCancel = findViewById(R.id.cancelButton);
+        etFirstName = findViewById(R.id.firstName);
+        etLastName = findViewById(R.id.lastName);
+        etEmail = findViewById(R.id.email);
+        etPassword = findViewById(R.id.password);
+        bProceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isAllFieldsChecked = CheckAllFields();
+                if (isAllFieldsChecked) {
+                    Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(i);
+                }
+            }
+        });
+        bCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.finish();
+                System.exit(0);
+            }
+        });
+    }
+    private boolean CheckAllFields() {
+        if (etFirstName.length() == 0) {
+            etFirstName.setError("This field is required");
+            return false;
+        }
+
+        if (etLastName.length() == 0) {
+            etLastName.setError("This field is required");
+            return false;
+        }
+
+        if (etEmail.length() == 0) {
+            etEmail.setError("Email is required");
+            return false;
+        }
+        if (etPassword.length() == 0) {
+            etPassword.setError("Password is required");
+            return false;
+        } else if (etPassword.length() < 8) {
+            etPassword.setError("Password must be minimum 8 characters");
+            return false;
+        }
+
+        // after all validation return true.
+        return true;
     }
 }
