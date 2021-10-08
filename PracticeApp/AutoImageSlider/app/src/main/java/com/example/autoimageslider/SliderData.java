326178eeea259e8cc7e294e0454c99fe664eb496
Md Raziullah Khan <raziullahkhan2222@gmail.com>
2021-10-08 08:28:02 +0530

    Added Model class SliderData

---
+++ PracticeApp/AutoImageSlider/app/src/main/java/com/example/autoimageslider/SliderData.java
@@ -0,0 +1,21 @@
+package com.example.autoimageslider;
+
+public class SliderData {
+
+    private int imgRes;
+
+    // Constructor method.
+    public SliderData(int imgRes) {
+        this.imgRes = imgRes;
+    }
+
+    // Getter method
+    public int getImgRes() {
+        return imgRes;
+    }
+
+    // Setter method
+    public void setImgRes(int imgRes) {
+        this.imgRes = imgRes;
+    }
+}
