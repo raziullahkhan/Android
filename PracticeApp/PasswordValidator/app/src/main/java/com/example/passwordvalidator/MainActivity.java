85a7e31e5863d55360ec711f4d23eb0b1b71935a
Md Raziullah Khan <raziullahkhan2222@gmail.com>
2021-10-08 07:55:53 +0530

    Implemented Password Validator

--- PracticeApp/PasswordValidator/app/src/main/java/com/example/passwordvalidator/MainActivity.java
+++ PracticeApp/PasswordValidator/app/src/main/java/com/example/passwordvalidator/MainActivity.java
@@ -3,12 +3,73 @@ package com.example.passwordvalidator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^" +
+                    "(?=.*[@#$%^&+=])" +
+                    "(?=\\S+$)" +
+                    ".{4,}" +
+                    "$");
+    private TextInputLayout email;
+    private TextInputLayout password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+    }
+    private boolean validateEmail() {
+        String emailInput = email.getEditText().getText().toString().trim();
+        if (emailInput.isEmpty()) {
+            email.setError("Field can not be empty");
+            return false;
+        }
+
+        else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            email.setError("Please enter a valid email address");
+            return false;
+        } else {
+            email.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        String passwordInput = password.getEditText().getText().toString().trim();
+
+        if (passwordInput.isEmpty()) {
+            password.setError("Field can not be empty");
+            return false;
+        }
+
+        else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
+            password.setError("Password is too weak");
+            return false;
+        } else {
+            password.setError(null);
+            return true;
+        }
+    }
+
+    public void confirmInput(View v) {
+        if (!validateEmail() | !validatePassword()) {
+            return;
+        }
+
+        String input = "Email: " + email.getEditText().getText().toString();
+        input += "\n";
+        input += "Password: " + password.getEditText().getText().toString();
+        Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
     }
 }
