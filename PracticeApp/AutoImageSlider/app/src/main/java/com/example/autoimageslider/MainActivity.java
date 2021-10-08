a96ada3b4df9fcec6221b4e13a3e9673fdf66fff
Md Raziullah Khan <raziullahkhan2222@gmail.com>
2021-10-08 08:46:56 +0530

    Implemented Auto Image Slider

--- PracticeApp/AutoImageSlider/app/src/main/java/com/example/autoimageslider/MainActivity.java
+++ PracticeApp/AutoImageSlider/app/src/main/java/com/example/autoimageslider/MainActivity.java
@@ -4,11 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.smarteist.autoimageslider.SliderView;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<SliderData> sliderDataArrayList = new ArrayList<>();
+
+        SliderView sliderView = findViewById(R.id.slider);
+
+        sliderDataArrayList.add(new SliderData(R.drawable.a1));
+        sliderDataArrayList.add(new SliderData(R.drawable.a2));
+        sliderDataArrayList.add(new SliderData(R.drawable.a3));
+
+        SliderAdapter adapter = new SliderAdapter(this, sliderDataArrayList);
+
+        sliderView.setAutoCycleDirection(SliderView.LAYOUT_DIRECTION_LTR);
+
+        sliderView.setSliderAdapter(adapter);
+
+        sliderView.setScrollTimeInSec(3);
+
+        sliderView.setAutoCycle(true);
+
+        sliderView.startAutoCycle();
     }
 }
