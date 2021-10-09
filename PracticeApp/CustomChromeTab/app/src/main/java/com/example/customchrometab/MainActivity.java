aed28d1d59da34c97802b936a314131ecdd77e7e
Md Raziullah Khan <raziullahkhan2222@gmail.com>
2021-10-09 19:31:16 +0530

    Implemented custom chrome layout

--- PracticeApp/CustomChromeTab/app/src/main/java/com/example/customchrometab/MainActivity.java
+++ PracticeApp/CustomChromeTab/app/src/main/java/com/example/customchrometab/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.customchrometab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.browser.customtabs.CustomTabsIntent;
+import androidx.core.content.ContextCompat;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button customChromeTabBtn;
+    String url = "https://www.google.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        customChromeTabBtn = findViewById(R.id.idBtnCustomChromeTab);
+        customChromeTabBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                CustomTabsIntent.Builder customIntent = new CustomTabsIntent.Builder();
+
+                customIntent.setToolbarColor(ContextCompat.getColor(MainActivity.this, R.color.purple_200));
+
+                openCustomTab(MainActivity.this, customIntent.build(), Uri.parse(url));
+            }
+        });
+    }
+    public static void openCustomTab(Activity activity, CustomTabsIntent customTabsIntent, Uri uri) {
+
+        String packageName = "com.android.chrome";
+        if (packageName != null) {
+
+            customTabsIntent.intent.setPackage(packageName);
+
+            customTabsIntent.launchUrl(activity, uri);
+        } else {
+            activity.startActivity(new Intent(Intent.ACTION_VIEW, uri));
+        }
     }
 }
